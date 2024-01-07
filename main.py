2381aadd96c8ed57746e9661e390c3e842c09e0f
Stanislav Dzasokhov <kurtat_programmer@mail.ru>
2024-01-07 17:08:13 +0300

    Add code

---
+++ main.py
@@ -0,0 +1,57 @@
+from playwright.sync_api import sync_playwright, expect
+from bs4 import BeautifulSoup
+from typing import Union, Optional
+import time
+import re
+
+
+class TheatreParse:
+    def __init__(self):
+        self.URL = 'https://teatr15.ru/'
+        self.page = None
+        self.context = None
+
+    def __get_iframe_id(self, html: str) -> Optional[str]:
+        soup = BeautifulSoup(html, 'lxml')
+        iframes = soup.find_all('iframe')
+
+        if len(iframes):
+            for iframe in iframes:
+                if "https://radario.ru/customer/afisha" in iframe.get('src'):
+                    return iframe.get('id')
+        return None
+
+    def parse(self):
+        with sync_playwright() as playwright:
+            browser = playwright.chromium.launch(headless=False)
+            self.context = browser.new_context()
+            self.page = self.context.new_page()
+            self.page.goto(self.URL)
+
+            self.page.get_by_role("link", name="Купить билет").click()
+            self.page.wait_for_timeout(5_000)
+            self.page.mouse.wheel(0, 150_000)
+            self.page.screenshot(path="step1.png")
+
+            is_exists_btn_load_more = self.page.frame_locator("#RadarioIframe2").get_by_role(
+                "button",
+                name="Загрузить еще").is_visible()
+            screenshot_counter = 2
+
+            task_iframe_id = self.__get_iframe_id(self.page.content())
+
+            while is_exists_btn_load_more:
+                self.page.frame_locator(f"#{task_iframe_id}").get_by_role("button", name="Загрузить еще").click()
+                self.page.wait_for_timeout(5_000)
+                self.page.mouse.wheel(0, 150_000)
+                self.page.screenshot(path=f"step{screenshot_counter}.png")
+                screenshot_counter += 1
+                is_exists_btn_load_more = self.page.frame_locator("#RadarioIframe2").get_by_role(
+                    "button",
+                    name="Загрузить еще").is_visible()
+
+            self.page.wait_for_timeout(7_000)
+
+
+if __name__ == '__main__':
+    TheatreParse().parse()
